17db9cc5c1cec0175dd0d75f42c06ceca9ef5355
Hitesh Mahto <hiteshmahto786@gmail.com>
2024-07-16 07:47:30 +0530

    Day 1

---
+++ Day 01 - Variables and Data Types/activites.js
@@ -0,0 +1,32 @@
+// Activity 1: Variable Declaration
+var number = 12;
+console.log(number);
+let firstName = "Hitesh";
+console.log(firstName);
+
+// Activity 2: Constant Declaration
+const isCommitted = true;
+console.log(isCommitted);
+
+// Activity 3: Data Types
+let num1 = 23.34;
+console.log(typeof num1);
+
+let friendName = "Neha";
+console.log(typeof friendName);
+
+var isOnline = false;
+console.log(typeof isOnline);
+
+const pet = {
+  name: "Golu",
+  age: 2,
+};
+console.log(typeof pet);
+
+const arr = [1, "hey", 1.3, null, undefined, [10, 20]];
+console.log(typeof arr);
+
+// Activity 4: Reassigning Variables
+
+// Activity 5: Understanding const
